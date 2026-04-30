d2bcc5a22793f60f7c320ec85f9b27987ef59233
yajatkaul <mc.crafter1973@gmail.com>
2026-04-30 23:55:55 +0530

    Addded particles for AbilityUsage and BagItem usage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 # 1.8.3+1.7.3-release
 - Fixed terapagos and ogerpon tera type being changed
+- Addded particles for AbilityUsage and BagItem usage
---
+++ common/src/main/java/com/github/yajatkaul/mega_showdown/mixin/battle/instructions/AbilityInstructionMixin.java
@@ -0,0 +1,34 @@
+package com.github.yajatkaul.mega_showdown.mixin.battle.instructions;
+
+import com.cobblemon.mod.common.api.battles.interpreter.BattleMessage;
+import com.cobblemon.mod.common.api.battles.model.PokemonBattle;
+import com.cobblemon.mod.common.battles.interpreter.instructions.AbilityInstruction;
+import com.cobblemon.mod.common.battles.pokemon.BattlePokemon;
+import com.cobblemon.mod.common.pokemon.Pokemon;
+import com.github.yajatkaul.mega_showdown.api.codec.Effect;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mixin(AbilityInstruction.class)
+public class AbilityInstructionMixin {
+    @Final @Shadow
+    private BattleMessage message;
+
+    @Inject(method = "invoke", at = @At("TAIL"))
+    private void invokeInject(PokemonBattle battle, CallbackInfo ci) {
+        BattlePokemon battlePokemon = message.battlePokemon(0, battle);
+        Pokemon pokemon = battlePokemon.getEffectedPokemon();
+
+        if (pokemon != null) {
+            String abilityName = "ability_" + pokemon.getAbility().getName();
+            Effect.getEffect(abilityName).applyEffectsBattle(pokemon, List.of(), Optional.empty(), null, battlePokemon);
+        }
+    }
+}
