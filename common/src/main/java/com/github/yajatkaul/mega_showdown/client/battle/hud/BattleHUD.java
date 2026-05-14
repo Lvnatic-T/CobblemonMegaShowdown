4e092fb927e499386e95ef12681fe557ebcedad0
Provismet <17149901+Provismet@users.noreply.github.com>
2026-05-14 15:32:18 +0100

    Fix HUD wonkyness again :(

--- common/src/main/java/com/github/yajatkaul/mega_showdown/client/battle/hud/BattleHUD.java
+++ common/src/main/java/com/github/yajatkaul/mega_showdown/client/battle/hud/BattleHUD.java
@@ -73,7 +73,10 @@ public class BattleHUD {
                         .findAny();
 
                 if (anyActor.isEmpty()) return; // This should not happen!
-                boolean isLeft = battle.getSpectating() && anyActor.get().getSide().equals(battle.getSide2());
+
+                boolean isLeft;
+                if (battle.getSpectating()) isLeft = anyActor.get().getSide().equals(battle.getSide2());
+                else isLeft = anyActor.get().getSide().equals(battle.getSide1());
 
                 for (BattlePokemonState battlePokemonState : side.getPokemon()) {
                     BattlePokemonMemory pokemon = memory.get(battlePokemonState.uuid());
